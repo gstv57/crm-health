e9b5d66835acfdeac93bda7b7ba25288dcd7a63b
Gustavo de Moraes <gustavodemoraes@plugwin.net>
2024-10-09 17:03:37 -0300

    wip: mudança no enum de ConsultaStatus

--- app/Enum/Consulta/ConsultaStatusEnum.php
+++ app/Enum/Consulta/ConsultaStatusEnum.php
@@ -4,11 +4,12 @@ namespace App\Enum\Consulta;
 
 enum ConsultaStatusEnum: string
 {
-    case AGENDADA       = 'Agendada';
-    case REALIZADA      = 'Realizada';
-    case CANCELADA      = 'Cancelada';
-    case REMARCADA      = 'Remarcada';
-    case ANDAMENTO      = 'Em andamento';
-    case CONFIRMADA     = 'Confirmada';
-    case NAO_COMPARECEU = 'Nao compareceu';
+    case AGENDADA             = 'Agendada';
+    case REALIZADA            = 'Realizada';
+    case CANCELADA            = 'Cancelada';
+    case REMARCADA            = 'Remarcada';
+    case ANDAMENTO            = 'Em andamento';
+    case CONFIRMADA           = 'Confirmada';
+    case NAO_COMPARECEU       = 'Nao compareceu';
+    case AGUARDANDO_PAGAMENTO = 'Aguardando pagamento';
 }
